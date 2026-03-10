907246868bca3d70dbbb039a81b61c479a2227aa
radityabudimansyah-star <radityabudimansyah@gmail.com>
2026-03-10 13:32:45 +0700

    Remove start.sh use railway.json command

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-DJANGO_DIR=/app/wms_project/django_backend
-python $DJANGO_DIR/manage.py migrate --pythonpath $DJANGO_DIR --settings settings
-exec gunicorn --bind 0.0.0.0:$PORT --pythonpath $DJANGO_DIR wsgi:application
